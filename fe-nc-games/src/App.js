4af3e9fd978c214db389f1cb967f4a0c36ec77dd
rodionross <100623210+rodionross@users.noreply.github.com>
2022-11-02 12:03:11 +0000

    ticket-6 finished

--- fe-nc-games/src/App.js
+++ fe-nc-games/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Reviews } from "./components/reviews/Reviews";
 import { Navbar } from "./components/navbar/Navbar";
 import { Home } from "./components/home/Home";
+import { SingleReview } from "./components/review/SingleReview";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/review/:id" element={<SingleReview />} />
           <Route path="/reviews/categories" element={<Reviews />} />
         </Routes>
       </div>

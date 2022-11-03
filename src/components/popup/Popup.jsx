bbdf18ce1c49af271eccb9155e8cdcfec4f7ba3b
rodionross <100623210+rodionross@users.noreply.github.com>
2022-11-03 15:51:55 +0000

    ticket-10 finished and user can now be logged in inside the popup

--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -1,16 +1,25 @@
+import { Login } from "../users/Login";
 import "./Popup.css";
+import { useContext } from "react";
+import { ActiveUserContext } from "../contexts/UserContext";
 
 export const Popup = ({ msg, whenClicked }) => {
+  const { activeUser } = useContext(ActiveUserContext);
+
   const handleClick = () => {
     whenClicked();
+    console.log("clicked");
   };
 
   return (
     <section className="popup-container">
       <div className="popup-inside">
-        <h2 className="popup-msg">{msg}</h2>
+        <h2 className="popup-msg">
+          {activeUser ? `Logged in as ${activeUser.username}` : msg}
+        </h2>
+        <Login />
         <button onClick={handleClick} className="popup-btn">
-          OK
+          {activeUser ? "CLOSE" : "OK"}
         </button>
       </div>
     </section>
